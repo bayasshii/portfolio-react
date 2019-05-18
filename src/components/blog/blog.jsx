42bc9a1ab1400343bdeecc08c8bf52b1b649e4a4
bayasshii <42494214+bayasshii@users.noreply.github.com>
2019-05-18 18:11:27 +0900

    make__component

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import blogComponents from "./contents/blogComponents.jsx";
 
 class Blog extends Component {
   render() {
     return (
     <div>
-      ブログ
+      <blogComponents />
     </div>
     );
   }
