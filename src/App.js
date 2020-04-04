dc872e210acafe2229fc556b66fa19b129dea21c
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-04-04 19:39:35 +0900

    twitter対応

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Helmet } from "react-helmet";
 import Header from "./components/header/header.jsx"
 import Footer from "./components/footer/footer.jsx"
 import './css/share.css'
@@ -9,6 +10,16 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Helmet
+          title={'きばやしのポートフォリオ'}
+          meta={[
+            { name: 'twitter:card', content: 'summary' },
+            { property: 'og:image', content: 'https://treeethreee.s3-ap-northeast-1.amazonaws.com/portfolioForTwitter.jpg' },
+            { property: 'og:title', content: 'きばやしのポートフォリオ' },
+            { property: 'og:description', '学生時代に作った作品を載せています' },
+            { property: 'og:url', content: 'https://kibachan.com' }
+          ]}
+        />
         <main>
           {this.props.children}
         </main>
